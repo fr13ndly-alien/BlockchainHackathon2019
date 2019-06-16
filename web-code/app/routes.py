5653d1f7f470249e1da648e81292fd311a5fac5e
osboxes.org <nhanitiu@gmail.com>
2019-06-16 05:52:51 -0400

    Add simple file upload feature

--- web-code/app/routes.py
+++ web-code/app/routes.py
@@ -1,7 +1,44 @@
+import os
+#import magic
+import urllib.request
 from app import app
+from flask import Flask, flash, request, redirect, render_template
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/')
 @app.route('/index')
 
 def index():
 	return "Hello, World!"
+
+@app.route('/upload_form')
+def upload_form():
+	return render_template('simple_upload.html')
+
+@app.route('/', methods=['POST'])
+def upload_file():
+	if request.method == 'POST':
+        # check if the post request has the file part
+		if 'file' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
+		file = request.files['file']
+		if file.filename == '':
+			flash('No file selected for uploading')
+			return redirect(request.url)
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			basedir = os.path.abspath(os.path.dirname(__file__))
+			print(basedir)
+			file.save(os.path.join(basedir, app.config['UPLOAD_FOLDER'], filename))
+			flash('File successfully uploaded')
+			return redirect('/')
+		else:
+			flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+			return redirect(request.url)
